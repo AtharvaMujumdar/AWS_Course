93fa4faf55d0a6444ce314e1872b1aac8d579fd8
AtharvaEpam <atharvamukul_mujumdar@epam.com>
2025-03-12 21:32:57 +0530

    add solution

--- task08/app/lambdas/uuid_generator/index.js
+++ task08/app/lambdas/uuid_generator/index.js
@@ -1,8 +1,32 @@
+const AWS = require("aws-sdk");
+const { v4: uuidv4 } = require("uuid");
+
+// Initialize S3 client
+const s3 = new AWS.S3();
+const BUCKET_NAME = "uuid-storage";
+
 exports.handler = async (event) => {
-    // TODO implement
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
-    };
-    return response;
+    try {
+        
+        const fileName = new Date().toISOString() + ".json";
+
+        
+        const uuidList = Array.from({ length: 10 }, () => uuidv4());
+
+        
+        const fileContent = JSON.stringify({ ids: uuidList }, null, 4);
+
+        
+        await s3.putObject({
+            Bucket: BUCKET_NAME,
+            Key: fileName,
+            Body: fileContent,
+            ContentType: "application/json"
+        }).promise();
+
+        return { status: "success", file: fileName };
+    } catch (error) {
+        console.error("Error uploading to S3:", error);
+        return { status: "error", message: error.message };
+    }
 };
